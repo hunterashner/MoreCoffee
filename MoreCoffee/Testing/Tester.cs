604773450a2b4a7f4eaaba9034f6938712b67558
hunterashner <hunterashner@users.noreply.github.com>
2022-06-30 13:10:08 -0400

    Blastoff

---
+++ MoreCoffee/Testing/Tester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace MoreCoffee.Testing
+{
+    class Tester
+    {
+        public Tester(string text)
+        {
+            Debug.Write(text);
+        }
+    }
+}
